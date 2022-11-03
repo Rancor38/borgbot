a6cee890f1483f1228e22b15801c4621236ca86e
Rancor38 <zak.rancor@hotmail.com>
2022-11-02 19:49:09 -0700

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+const {Client, Events, GatewayIntentBits, Application} = require('discord.js')
+const { token } = require('./config.json')
+
+const PORT = process.env.PORT || 3030;
+
+const prefix = 'm'
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ],
+});
+
+client.once(Events.ClientReady, () => {
+    console.log('Ready!')
+})
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return
+    
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if (command === 'test') {
+        message.channel.send('Bot is working!')
+    }
+    else if (command == 'ork') {
+        message.channel.send('borg')
+    }
+})
+
+
+client.login(token)
