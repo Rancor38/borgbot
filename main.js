1e738314f64d6d20fc5ce860d83af8a87b651db2
Rancor38 <zak.rancor@hotmail.com>
2023-03-28 13:08:47 -0700

    Added pigcoin function

--- main.js
+++ main.js
@@ -1,112 +1,99 @@
-const { Client, Events, GatewayIntentBits, Application } = require('discord.js')
+const {
+    Client,
+    Events,
+    GatewayIntentBits,
+    Application,
+} = require("discord.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const fetch = require("node-fetch");
-const apiController = require('./controllers/apiController');
-const { response } = require('express');
+const { includeWords, removeBorgbot } = require("./lib/functions");
+const {
+    convertToGold,
+} = require("./applications/pigCoinApp")
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use("/api/generate", apiController )
-
-const commands = ['borgbot', 'borgbot,', 'mork', 'mark', 'zak']
-
-function includeWords(str, arr) {
-    const words = str.toLowerCase().split(/[^\w]+/); // split the string into lowercase words
-    for (let i = 0; i < arr.length; i++) { // loop over the array of words
-      if (words.includes(arr[i].toLowerCase())) { // check if the word is included in the string
-        return true; // if found, return true
-      }
-    }
-    return false; // if not found, return false
-}
-  
-function removeBorgbot(str) {
-    const arr = str.split('borgbot');
-    if (arr[arr.length - 1] === '') {
-      // If the last element is an empty string, remove it
-      arr.pop();
-    }
-    return arr.join('borgbot');
-  }
-  
+const commands = ["borgbot", "borgbot,", "mork", "mark", "zak", "pigcoins"];
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
     ],
 });
 
 client.once(Events.ClientReady, () => {
-    console.log('Ready!')
-})
+    console.log("Borgbot is ready, firing on all cylinders, Brrrrrrrr!");
+});
 
-client.on('messageCreate', message => {
+client.on("messageCreate", (message) => {
     //if the bot wrote the message, ignore
     if (message.author.bot) {
-        return
+        return;
     }
     //if the messages doesn't include a command, ignore
     if (!includeWords(message.content, commands)) {
-        return
+        return;
     }
-    
-    const args = message.content
+
+    const args = message.content;
     // console.log(args + " is args")
-    const command = args.toLowerCase()
+    const command = args.toLowerCase();
 
     if (command.includes("test")) {
-        message.channel.send('Bot is working!')
-    }
-    else if (command.includes('mork')) {
-        message.channel.send('*borg*')
-    }
-    else if (command.includes('mark')) {
-        message.channel.send('*barg*')
-    }
-    else if (command.includes('zak')) {
-        message.channel.send('*balg*')
-    }
-    else if (command === 'borgbot' || 'borgbot,') {
+        message.channel.send("Bot is working!");
+    } else if (command.includes("mork")) {
+        message.channel.send("*borg*");
+    } else if (command.includes("mark")) {
+        message.channel.send("*barg*");
+    } else if (command.includes("zak")) {
+        message.channel.send("*balg*");
+    } else if (command.includes("pigcoins")) {
+        const quantity = message.content.split(' ')[1]
+        if (Number(quantity) > 0) {
+            message.channel.send(convertToGold(quantity))
+        }
+        console.log(quantity)
+    } else if (command === "borgbot" || "borgbot,") {
         // console.log("borghit " + args)
-            const PROMPT = "Respond to the following prompt using only a randomly selected series of words from the following array (the response can repeat words from the array multiple times) array: [mork, mork, mork, mork, mork, Mork, mork Mork, MORK, borg, borg, borg, borg, borg, Barg, BARG, balg]. Prompt: " + removeBorgbot(args)
-            // console.log(PROMPT + " is PROMPT");
-            const raw = JSON.stringify({
-                model: "text-davinci-003",
-                prompt: PROMPT,
-                temperature: 0.5,
-                max_tokens: 15,
-                n: 3,
-            });
-        
-            const requestOptions = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-                },
-                body: raw,
-                redirect: "follow",
-            };
-        
-            try {
-                fetch("https://api.openai.com/v1/completions", requestOptions)
-                  .then((response) => response.json())
-                  .then((json) => {
-                    message.channel.send(     
-                              json.choices[0].text
-                      )
-                  });
-              } catch (error) {
-                console.error(error);
-              }
-    }
-})
+        const PROMPT =
+            "Respond to the following prompt using only a randomly selected series of words from the following array (the response can repeat words from the array multiple times) array: [mork, mork, mork, mork, mork, Mork, mork Mork, MORK, borg, borg, borg, borg, borg, Barg, BARG, balg]. Prompt: " +
+            removeBorgbot(args);
+        // console.log(PROMPT + " is PROMPT");
+        const raw = JSON.stringify({
+            model: "text-davinci-003",
+            prompt: PROMPT,
+            temperature: 0.5,
+            max_tokens: 15,
+            n: 3,
+        });
 
-client.login(process.env.token)
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+            body: raw,
+            redirect: "follow",
+        };
+
+        try {
+            fetch("https://api.openai.com/v1/completions", requestOptions)
+                .then((response) => response.json())
+                .then((json) => {
+                    message.channel.send(json.choices[0].text);
+                });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
+
+client.login(process.env.token);
