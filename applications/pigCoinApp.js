1723ef8ffd942de97566ae9c2081ab3e8e73fbe3
Rancor38 <zak.rancor@hotmail.com>
2023-03-28 17:17:29 -0700

    made format edits

--- applications/pigCoinApp.js
+++ applications/pigCoinApp.js
@@ -1,52 +1,48 @@
-
-
-
 const pigCoinSeparator = (totalPigCoins) => {
-    const money = {
-        gold: 0,
-        silver: 0,
-        copper: 0,
-    }
-    let randomNumber = Math.floor(Math.random() * totalPigCoins);
-    if (totalPigCoins > randomNumber) {
-        money.gold = totalPigCoins - randomNumber;
-    } else {
-        money.gold = randomNumber - totalPigCoins;
-    }
-    let secondRandomNumber = Math.floor(Math.random() * randomNumber);
-    if (randomNumber > secondRandomNumber) {
-        money.silver = randomNumber - secondRandomNumber;
-        money.copper = secondRandomNumber;
-    } else {
-        money.silver = secondRandomNumber - randomNumber;
-        money.copper = randomNumber;
-    }
-    return(money)
-}
+        const money = {
+                gold: 0,
+                silver: 0,
+                copper: 0,
+        };
+        let randomNumber = Math.floor(Math.random() * totalPigCoins);
+        if (totalPigCoins > randomNumber) {
+                money.gold = totalPigCoins - randomNumber;
+        } else {
+                money.gold = randomNumber - totalPigCoins;
+        }
+        let secondRandomNumber = Math.floor(Math.random() * randomNumber);
+        if (randomNumber > secondRandomNumber) {
+                money.silver = randomNumber - secondRandomNumber;
+                money.copper = secondRandomNumber;
+        } else {
+                money.silver = secondRandomNumber - randomNumber;
+                money.copper = randomNumber;
+        }
+        return money;
+};
 
 const convertToGold = (numberofPigCoins) => {
-    const money = pigCoinSeparator(numberofPigCoins);
-    
-    let holdValue = money.copper;
-    money.copper = money.copper % 10;
-    holdValue = (holdValue - money.copper) / 10;
-    money.silver += holdValue;
-    // console.log(`copper is ${money.copper} and holdValue is ${holdValue} silver is ${money.silver}`)
-    
-    holdValue = money.silver;
-    money.silver = money.silver % 10;
-    holdValue = (holdValue - money.silver) / 10;
-    money.gold += holdValue;
-    // console.log(`silver is ${money.silver} and holdValue is ${holdValue} gold is ${money.gold}`)
-    
-    // console.log(`Gold: ${money.gold}\nSilver: ${money.silver}\nCopper: ${money.copper}`)
-    return `Your pigcoins returned you: ${money.gold} gp, ${money.silver} sp, ${money.copper} cp`
-    silverValue.innerText = money.silver;
-    copperValue.innerText = money.copper;
-}
+        const money = pigCoinSeparator(numberofPigCoins);
 
+        let holdValue = money.copper;
+        money.copper = money.copper % 10;
+        holdValue = (holdValue - money.copper) / 10;
+        money.silver += holdValue;
+        // console.log(`copper is ${money.copper} and holdValue is ${holdValue} silver is ${money.silver}`)
+
+        holdValue = money.silver;
+        money.silver = money.silver % 10;
+        holdValue = (holdValue - money.silver) / 10;
+        money.gold += holdValue;
+        // console.log(`silver is ${money.silver} and holdValue is ${holdValue} gold is ${money.gold}`)
+
+        // console.log(`Gold: ${money.gold}\nSilver: ${money.silver}\nCopper: ${money.copper}`)
+        return `Your pigcoins returned you: ${money.gold} gp, ${money.silver} sp, ${money.copper} cp`;
+        silverValue.innerText = money.silver;
+        copperValue.innerText = money.copper;
+};
 
 module.exports = {
-pigCoinSeparator,
-convertToGold
-}
+        pigCoinSeparator,
+        convertToGold,
+};
--- main.js
+++ main.js
@@ -1,98 +1,109 @@
+const { Client, Events, GatewayIntentBits, Application } = require("discord.js")
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+require("dotenv").config()
+const fetch = require("node-fetch")
 const {
-    Client,
-    Events,
-    GatewayIntentBits,
-    Application,
-} = require("discord.js");
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-require("dotenv").config();
-const fetch = require("node-fetch");
-const { includeWords, removeBorgbot } = require("./lib/functions");
-const {
-    convertToGold,
-} = require("./applications/pigCoinApp")
+        includeWords,
+        removeBorgbot,
+        State,
+        setPrompt,
+} = require("./lib/functions")
+const { convertToGold } = require("./applications/pigCoinApp")
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-const commands = ["borgbot", "borgbot,", "mork", "mark", "zak", "pigcoins", 'bottest'];
+const commands = [
+        "borgbot",
+        "borgbot,",
+        "mork",
+        "mark",
+        "zak",
+        "pigcoins",
+        "bottest",
+]
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
-});
+        intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMembers,
+        ],
+})
 
 client.once(Events.ClientReady, () => {
-    console.log("Borgbot is ready, firing on all cylinders, Brrrrrrrr!");
-});
+        console.log("Borgbot is ready, firing on all cylinders, Brrrrrrrr!")
+})
 
 client.on("messageCreate", (message) => {
-    //if the bot wrote the message, ignore
-    if (message.author.bot) {
-        return;
-    }
-    //if the messages doesn't include a command, ignore
-    if (!includeWords(message.content, commands)) {
-        return;
-    }
-
-    const args = message.content;
-    // console.log(args + " is args")
-    const command = args.toLowerCase();
-
-    if (command.includes("bottest")) {
-        message.channel.send("Bot is working!");
-    } else if (command.includes("mork")) {
-        message.channel.send("*borg*");
-    } else if (command.includes("mark")) {
-        message.channel.send("*barg*");
-    } else if (command.includes("zak")) {
-        message.channel.send("*balg*");
-    } else if (command.includes("pigcoins")) {
-        const quantity = message.content.split(' ')[1]
-        if (Number(quantity) > 0) {
-            message.channel.send(convertToGold(quantity))
+        //if the bot wrote the message, ignore
+        if (message.author.bot) {
+                return
         }
-        // console.log(quantity)
-    } else if (command.includes("borgbot")) {
-        console.log(args)
-        const PROMPT =
-            "Respond to the following prompt using only a randomly selected series of words from the following array (the response can repeat words from the array multiple times) array: [mork, mork, mork, mork, mork, Mork, mork Mork, MORK, borg, borg, borg, borg, borg, Barg, BARG, balg]"
-        console.log(PROMPT + " is PROMPT");
-        const raw = JSON.stringify({
-            model: "text-davinci-003",
-            prompt: PROMPT,
-            temperature: 0.5,
-            max_tokens: 20,
-            n: 3,
-        });
-
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-            },
-            body: raw,
-            redirect: "follow",
-        };
-
-        try {
-            fetch("https://api.openai.com/v1/completions", requestOptions)
-                .then((response) => response.json())
-                .then((json) => {
-                    message.channel.send(json.choices[0].text);
-                });
-        } catch (error) {
-            console.error(error);
+        //if the messages doesn't include a command, ignore
+        if (!includeWords(message.content, commands)) {
+                return
         }
-    }
-});
 
-client.login(process.env.token);
+        const args = message.content
+        // console.log(args + " is args")
+        const command = args.toLowerCase()
+
+        if (command.includes("bottest")) {
+                message.channel.send("Bot is working!")
+        } else if (command.includes("mork")) {
+                message.channel.send("*borg*")
+        } else if (command.includes("mark")) {
+                message.channel.send("*barg*")
+        } else if (command.includes("zak")) {
+                message.channel.send("*balg*")
+        } else if (command.includes("pigcoins")) {
+                const quantity = message.content.split(" ")[1]
+                if (Number(quantity) > 0) {
+                        message.channel.send(convertToGold(quantity))
+                }
+                // console.log(quantity)
+        } else if (command.includes("borgbot")) {
+                console.log(args)
+                const PROMPT =
+                        "Respond to the following prompt using only a randomly selected series of words from the following array (the response can repeat words from the array multiple times) array: [mork, mork, mork, mork, mork, Mork, mork Mork, MORK, borg, borg, borg, borg, borg, Barg, BARG, balg]"
+                console.log(PROMPT + " is PROMPT")
+                const raw = JSON.stringify({
+                        model: "text-davinci-003",
+                        prompt: PROMPT,
+                        temperature: 0.5,
+                        max_tokens: 20,
+                        n: 3,
+                })
+
+                const requestOptions = {
+                        method: "POST",
+                        headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                        },
+                        body: raw,
+                        redirect: "follow",
+                }
+
+                try {
+                        fetch(
+                                "https://api.openai.com/v1/completions",
+                                requestOptions
+                        )
+                                .then((response) => response.json())
+                                .then((json) => {
+                                        message.channel.send(
+                                                json.choices[0].text
+                                        )
+                                })
+                } catch (error) {
+                        console.error(error)
+                }
+        }
+})
+
+client.login(process.env.token)
