fefd3a017cc78eaed46d292cfd9f898416ad165e
Zakariah <zak.rancor@hotmail.com>
2024-03-04 17:25:54 -0800

    r

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ client.on("messageCreate", async (message) => {
 		message.channel.send("*You're still here?*")
 	}
 	if (command.includes("kat")) {
-		message.channel.send("*prrrrrrrrrr* ♥")
+		message.channel.send("*prrrrrrrrrrr* ♥")
 	}
 	if (command.includes("egg")) {
 		message.channel.send("🥚")
