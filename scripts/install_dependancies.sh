acc79dc7e74769a01a89af4eeb7024b1406e308a
zakariah <zak.rancor@hotmail.com>
2024-12-04 20:39:45 -0800

    Prepare aws deploy

---
+++ scripts/install_dependancies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/ec2-user/discord-bot
+
+# Install dependencies
+npm install
---
+++ scripts/start_bot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /home/ec2-user/discord-bot
+
+# Start the bot
+npm start
